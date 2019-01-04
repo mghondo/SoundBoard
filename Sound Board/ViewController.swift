ad0fd16eeffa1d1b3c0731e892a98e92da33836a
Morgan Hondros <morganhondros@gmail.com>
2019-01-04 14:58:50 -0500

    First Commit

---
+++ Sound Board/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound Board
+//
+//  Created by Morgan Hondros on 1/4/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
